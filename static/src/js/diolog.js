d121c2f6f6fd0c3bc71ff88b4c3c4b6850902a75
youyuhsuan <stellayou0118@gmail.com>
2024-06-06 05:12:09 +0800

    style:Align MRT name to the left and category to the right

--- static/src/js/diolog.js
+++ static/src/js/diolog.js
@@ -1,11 +1,28 @@
 const openDialogButton = document.querySelector(".open-dialog");
-const closeDialogButton = document.querySelector(".close-dialog");
+const closeDialogButton = document.querySelectorAll(".close-dialog");
 const signinDialog = document.querySelector(".signin-dialog");
+const signupDialog = document.querySelector(".signup-dialog");
+
+const signupLink = document.querySelector(".signup-link");
+const signinLink = document.querySelector(".signin-link");
 
 openDialogButton.addEventListener("click", () => {
   signinDialog.showModal();
 });
 
-closeDialogButton.addEventListener("click", () => {
+closeDialogButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    signinDialog.close();
+    signupDialog.close();
+  });
+});
+
+signupLink.addEventListener("click", () => {
   signinDialog.close();
+  signupDialog.showModal();
+});
+
+signinLink.addEventListener("click", () => {
+  signupDialog.close();
+  signinDialog.showModal();
 });
